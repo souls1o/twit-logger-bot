2ab19af3c34a0ea7b7c1015af84b80e505e8b773
souls1o <161530718+souls1o@users.noreply.github.com>
2024-07-12 03:43:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -208,7 +208,7 @@ async def links(update: Update, context: CallbackContext) -> None:
     if group_id == -4146400715:
         ls = ['https://www\.calendlly\.xyz/coingecko/invitation', 'https://www\.calendlly\.xyz/coinmarketcap/invitation', 'https://www\.calendlly\.xyz/bitcoinmagazine/invitation']
     elif group_id == -4148855237:
-        ls = ['https://www\.cointele\.site/cointelegraph/meeting-hour?month=2024-07']
+        ls = ['https://www\.cointele\.site/cointelegraph/meeting\-hour?month=2024\-07']
 
     print(f"({update.message.chat.title}) retrieved links: [{'] ['.join(ls) if ls else 'None :('}]")
     for link in ls:
