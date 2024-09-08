c01161fb9fa057b4729d346bf0d670eb9b031da1
souls1o <161530718+souls1o@users.noreply.github.com>
2024-09-08 11:27:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from telegram.ext import Application, CommandHandler, CallbackContext
 from keep_alive import keep_alive
 keep_alive()
 
-TELEGRAM_BOT_TOKEN = '6790216831:AAHbUIZKq38teKnZIw9zUQDRSD6csT-JEs4'
+TELEGRAM_BOT_TOKEN = ''
 
-TWITTER_CLIENT_ID = 'eWNUdkx4LTlnaGQ0N3BaSGJyYkU6MTpjaQ'
-TWITTER_CLIENT_SECRET = '4cct_4dZ3BVz_MNKKjazWi1M3XVelnSiGqV6R5hBxC-Pbj7ytn'
+TWITTER_CLIENT_ID = ''
+TWITTER_CLIENT_SECRET = ''
 
 
 async def start(update: Update, context: CallbackContext) -> None:
